58d1336cb9ed7b4b365b5d18825aefb077e52a74
Ryan Norin <103920941+NironR@users.noreply.github.com>
2022-09-21 10:58:10 +1000

    commit

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,13 +3,14 @@ import {useState, useEffect} from "react"
 import { Container, Row, Col } from "react-bootstrap"
 import { ArrowRightCircle } from 'react-bootstrap-icons';
 import headerImg from "./assets/img/header-img.svg"
+import './css/banner.css'
 
 const Banner = () => {
     const [loopNum, setLoopNum] = useState(0)
     const [IsDeleting, setIsDeleting] = useState(false)
     const [text, setText] = useState("")
     const [delta, setDelta] = useState("")
-    const toRotate = ["web Developer", "Web Designer", "UI/UX Designer"]
+    const toRotate = ["Web Developer", "Web Designer", "UI/UX Designer"]
     const period = 2000
 
     useEffect(() => {
@@ -23,6 +24,21 @@ const Banner = () => {
     function tick() {
         let i = loopNum % toRotate.length;
         let fullText = toRotate[i]
+        let updatedText = IsDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1)
+        setText(updatedText)
+
+        if (IsDeleting) {
+            setDelta(prevDelta => prevDelta / 2)
+        }
+
+        if (!IsDeleting && updatedText === fullText) {
+            setIsDeleting(true)
+            setDelta(period)
+        } else if (IsDeleting && updatedText === "") {
+            setIsDeleting(false)
+            setLoopNum(loopNum + 1)
+            setDelta(500)
+        }
     }
 
     return (
@@ -33,9 +49,9 @@ const Banner = () => {
                         <span className="tagline">
                             Welcome to my E-Portfolio
                         </span>
-                        <h1>{"Hi I'm Ryan Norin. "}
+                        <h1>{"Hi I'm Ryan Norin. A "}
                             <span className='wrap'>
-                                A Web Developer.
+                                {text}
                             </span>
                         </h1>
                         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Enim, eveniet, maiores et culpa suscipit nam vero iure odio nesciunt error incidunt eos? Cupiditate, nobis consequuntur doloribus facere autem voluptatibus reiciendis?</p>
