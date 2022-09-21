73af3626cd1b09c9c987507dffd5bb969f743018
Ryan Norin <103920941+NironR@users.noreply.github.com>
2022-09-21 14:47:56 +1000

    commit

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './components/NavBar.jsx';
 import Banner from './components/Banner.jsx'
 import Skills from './components/Skills.jsx';
+import Projects from './components/Projects.jsx'
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Navbar />
       <Banner />
       <Skills />
+      <Projects />
     </div>
   );
 }
