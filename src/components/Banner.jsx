3a4cdb8a709fe997959fa9a1e231dbe977dcbabb
Ryan Norin <103920941+NironR@users.noreply.github.com>
2022-09-27 20:15:08 +1000

    commit

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {useState, useEffect} from "react"
 import { Container, Row, Col } from "react-bootstrap"
 import { ArrowRightCircle } from 'react-bootstrap-icons';
-import headerImg from "./assets/img/header-img.svg"
+import headerImg from "./assets/img/kisspng-astronaut-drawing-royalty-free-astronauta-ni-ntilde-o-5b4fbbed233a42.2318924415319521091443.png"
 import './css/banner.css'
 import 'animate.css'
 import TrackVisibility from 'react-on-screen';
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,10 @@
 import { Container, Row, Col } from "react-bootstrap"
 import React from 'react';
 import './css/footer.css'
-import logo from "./assets/img/logo.svg"
+import logo from "./assets/img/logo.png"
 import navIcon1 from "./assets/img/nav-icon1.svg"
-import navIcon2 from "./assets/img/nav-icon2.svg"
-import navIcon3 from "./assets/img/nav-icon3.svg"
+import navIcon2 from "./assets/img/github.svg"
+import navIcon3 from "./assets/img/file-regular.svg"
 
 const Footer = () => {
     return (
@@ -15,11 +15,11 @@ const Footer = () => {
                 <img src={logo} alt="Logo" />
               </Col>
               <Col size={12} sm={6} className="text-center text-sm-end">
-                <div className="social-icon">
-                  <a href="#"><img src={navIcon1} alt="Icon" /></a>
-                  <a href="#"><img src={navIcon2} alt="Icon" /></a>
-                  <a href="#"><img src={navIcon3} alt="Icon" /></a>
-                </div>
+              <div className="social-icon">
+                <a href="https://www.linkedin.com/in/ryan-norin-4970a5249/" target="_blank" ><img src={navIcon1} alt="" /></a>
+                <a href="https://github.com/NironR" target="_blank"><img className="navIcon" src={navIcon2} alt="" /></a>
+                <a href="https://docs.google.com/document/d/1r8rGz8Nzbro6F5Zwm4RsV8mo5_gEUgCt/edit" target="_blank"><img className="navIcon" src={navIcon3} alt="" /></a>
+              </div>
                 <p>Copyright 2022. All Rights Reserved Ryan Norin</p>
               </Col>
             </Row>
